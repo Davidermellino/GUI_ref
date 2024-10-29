1b4caaf1f26f8ec6cd318d583d23297f80bfabbf
Davide Ermellino <d.ermellino@studenti.unica.it>
2024-10-29 01:07:43 +0100

    aggiunta logica per la presenza dell'occorrenza [cit] con la rispettiva apparizione del pulsante compute placeeholder in caso di 0 occorrenze. aggiunta logica del radio button nel Get references, se su status non fa nulla, nell'altrocaso appare textarea per scrivere citazioni

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("iniziamo");
+let getref = document.querySelector("#getref");
+let compute_placeholder = document.querySelector("#comp_placeholder");
+let container_provided_by_user_elements = document.querySelector("#ct_prov_by_user")
+let radios = document.querySelectorAll(".form-check-input");
+let textarea = document.querySelector("#first_ta");
+
+getref.addEventListener("click", ()=>{
+    let check = Array.from(radios).find((button) => button.checked);
+    
+    if (check.value == "prov"){
+        container_provided_by_user_elements.classList.remove("d-none")        
+    }
+    else{
+        container_provided_by_user_elements.classList.add("d-none")        
+    }
+})
+
+textarea.addEventListener("blur", ()=>{
+    if(textarea.value.includes("[cit]")){
+        compute_placeholder.classList.add("d-none")
+    }else{
+        compute_placeholder.classList.remove("d-none")
+        
+    }
+
+    
+})
