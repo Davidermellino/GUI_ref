1cacf6503322f5166d0c50b46dd716d7fe8b68bb
Davide Ermellino <d.ermellino@studenti.unica.it>
2024-10-30 01:22:11 +0100

    aggiungto un tooltip nel punto di domanda e iniziata la logica per estrarre i titoli dal get references

--- main.js
+++ main.js
@@ -1,39 +1,54 @@
-let getref = document.querySelector("#getref");
 let compute_placeholder = document.querySelector("#comp_placeholder");
-let container_provided_by_user_elements = document.querySelector("#ct_prov_by_user")
+let container_provided_by_user_elements =
+  document.querySelector("#ct_prov_by_user");
+
 let radios = document.querySelectorAll(".form-check-input");
 let textarea = document.querySelector("#first_ta");
 
-radios[0].addEventListener("change", ()=>{
+let getref = document.querySelector("#getref");
+let usr_ref = document.querySelector("#usr_ref");
 
-    container_provided_by_user_elements.classList.add("d-none")        
-})
+let dropdown = document.querySelector("#dropdownSelect");
+let format_option = document.querySelector("#format_option");
 
-radios[1].addEventListener("change", ()=>{
-    
-    
-    container_provided_by_user_elements.classList.remove("d-none")        
-   
-})
+// abilito tooltip
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
 
-textarea.addEventListener("blur", ()=>{
-    if(textarea.value.includes("[cit]")){
-        compute_placeholder.classList.add("d-none")
-    }else{
-        compute_placeholder.classList.remove("d-none")        
-    }
-
-})
-
-//roba guida
-let guideIcon = document.querySelector(".pd");
-let guideContent = document.querySelector('#guideContent');
-let closeGuide = document.querySelector('#closeGuide');
-
-guideIcon.addEventListener("click", () => {
-    guideContent.classList.remove("d-none");
+// veririco se [cit] occore nel textbox iniziale per far apparire compute placeholder o meno
+textarea.addEventListener("blur", () => {
+  if (textarea.value.includes("[cit]")) {
+    compute_placeholder.classList.add("d-none");
+  } else {
+    compute_placeholder.classList.remove("d-none");
+  }
 });
 
-closeGuide.addEventListener("click", () => {
-    guideContent.classList.add("d-none");
+// Verifico se i radio button status e prov cambiano per far apparire la sezione per l'inserimento
+radios.forEach((radio, index) => {
+  radio.addEventListener("change", () => {
+    if (index === 0) {
+      container_provided_by_user_elements.classList.add("d-none");
+    } else if (index === 1) {
+      container_provided_by_user_elements.classList.remove("d-none");
+    }
+  });
+});
+
+// stampo il titolo in base al formato della biografia
+getref.addEventListener("click", () => {
+  if (radios[1].checked) {
+    selected_format = dropdown.value
+    if (selected_format === "1") {
+      console.log("Hai selezionato lo stile APA.");
+    } else if (selected_format === "2") {
+      console.log("Hai selezionato Bibliatex.");
+    } else {
+      console.log("Seleziona un formato di riferimento.");
+    }
+  }
 });
