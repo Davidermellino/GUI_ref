518de63327e473056eb2f7f5b5563ca2f038f21e
Davide Ermellino <d.ermellino@studenti.unica.it>
2024-11-01 15:34:07 +0100

    creata la funzione per estrarre titoli, al momento li stampa nella casella result con il logo bibtex o apastyle affianco

--- function_js/extract_title.js
+++ function_js/extract_title.js
@@ -1,5 +1,7 @@
 export default function extract_titles(format) {   
     let titles =  []
+
+    
     if (format == 1) {
         let references = usr_ref.value.split("\n"); // separo le varie righe (una references per riga)
         references.forEach(reference => { 
@@ -7,6 +9,8 @@ export default function extract_titles(format) {
               titles.push(reference.match(/\*(.*?)\*/)?.[1]) //estraggo il titolo, al momento supponendo che sia dentro degli asterischi
             }
         });
+
+
     }else if(format == 2){
         let references = usr_ref.value.split("@"); // separo le varie righe (separatore chiocciola)
         references.forEach(reference => { 
