6f1f654f1d5b8948ec5d802bc770413e309740af
Davide Ermellino <d.ermellino@studenti.unica.it>
2024-11-21 11:11:31 +0100

    bottoni sempre visibili, gestione errore getcitation in caso di mancate occorrenze [cit]

--- function_js/cerca_[cit].js
+++ function_js/cerca_[cit].js
@@ -1,11 +1,5 @@
-export default function cerca_cit(textarea, compute_placeholder, div_getref ){
-    textarea.addEventListener("blur", () => {
-        if (textarea.value.includes("[cit]")) {
-          compute_placeholder.classList.add("d-none");
-          div_getref.classList.remove("d-none")
-        } else {
-          compute_placeholder.classList.remove("d-none");
-          div_getref.classList.add("d-none")    
-        }
-      });
+export default function cerca_cit(textarea ){
+        if (textarea.value.includes("[cit]")) 
+          return true
+        return false
 }
--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import extract_titles from "./function_js/extract_title.js";
 import print_result from "./function_js/printResult.js";
 import cerca_cit from "./function_js/cerca_[cit].js";
 
-
 let compute_placeholder = document.querySelector("#comp_placeholder");
 let container_provided_by_user_elements =
   document.querySelector("#ct_prov_by_user");
@@ -26,18 +25,17 @@ const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
 //abilito i popover
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-
-
-// veririco se [cit] occore nel textbox iniziale per far apparire compute placeholder o meno
-cerca_cit(textarea, compute_placeholder, div_getref)
-
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
 
 //l'utente ha cliccato compute placeholder (vengono aggiunti i cit) e lo segnalo
-compute_placeholder.addEventListener("click", () => { //al click di place holder
-  textarea.value += "[cit]"
-  div_getref.classList.remove("d-none")
+compute_placeholder.addEventListener("click", () => {
+  //al click di place holder
+  textarea.value += "[cit]";
 });
 
 // Verifico se i radio button status e prov cambiano per far apparire la sezione per l'inserimento
@@ -52,43 +50,63 @@ radios.forEach((radio, index) => {
 });
 
 // stampo i titoli in base al formato della biografia
-getref.addEventListener("click", () => { //al click di get references
-  let titles = []
+getref.addEventListener("click", () => {
+  //al click di get references
+  
+  console.log(cerca_cit(textarea));
 
-  let format = dropdown.value
-  if (radios[1].checked) { // se seleziona provided by user
-    titles = extract_titles(format)
-    result.innerHTML = ""; 
-    titles.forEach(title => {        
-            let div = document.createElement("div");
-            div.classList.add("row", "my-3", "border-bottom", "border-black");
-            div.innerHTML = `
+  if (cerca_cit(textarea)) {
+    let titles = [];
+    let format = dropdown.value;
+    if (radios[1].checked) {
+      // se seleziona provided by user
+      titles = extract_titles(format);
+      result.innerHTML = "";
+      titles.forEach((title) => {
+        let div = document.createElement("div");
+        div.classList.add("row", "my-3", "border-bottom", "border-black");
+        div.innerHTML = `
                 <div class="col-8 align-self-center">    
                         <span class="m-0 text-result"">${title}</span>
                 </div>
                 <div class="col-4 d-flex align-items-center justify-content-center">
-                    <img src=${format == 1 ? "./assets/APA_icon.jpeg" : "./assets/Bibtex_icon.jpg"} alt="" class="format-logo mb-2">
+                    <img src=${
+                      format == 1
+                        ? "./assets/APA_icon.jpeg"
+                        : "./assets/Bibtex_icon.jpg"
+                    } alt="" class="format-logo mb-2">
                 </div>
         `;
-            result.appendChild(div);
-    });
-  }
+        result.appendChild(div);
+      });
+    }
 
+    //selezionato status per GetRef
+    if (radios[0].checked) {
+      // se seleziona status
 
-  //selezionato status per GetRef
-  if (radios[0].checked) { // se seleziona status
+      fetch("risultato.json") // prendo dal file JSON i titoli
+        .then((response) => response.json())
+        .then((data) => {
+          // data.citazioni.forEach((citazione) => {
 
-    fetch("risultato.json") // prendo dal file JSON i titoli 
-      .then((response) => response.json())
-      .then((data) => {
+          //   titles.push(citazione.testo) //li aggiungo alla lista
 
-        // data.citazioni.forEach((citazione) => {
+          // });
 
-        //   titles.push(citazione.testo) //li aggiungo alla lista
-          
-        // });
-        
-        print_result(data.citazioni, 1) //format 0 = APE
-      })
+          print_result(data.citazioni, 1); //format 0 = APE
+        });
+    }
+  } else {
+    let div = document.createElement("div");
+    div.classList.add("alert", "alert-danger");
+    div.setAttribute("role", "alert");
+    div.innerHTML = `
+      Non hai inserito cit 
+    `;
+    textarea.insertAdjacentElement("afterend", div);  
+    setTimeout(() => {
+      div.remove(); // Rimuove l'alert dopo 5 secondi
+  }, 2000);
   }
 });
