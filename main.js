77f5ebdce76b4c6ad4779485e26f7fd506b2bde2
Luca <l.agus22@studenti.unica.it>
2024-11-22 17:17:00 +0100

    Gestito il caso in cui siano già presenti i [cit] + commenti + traduzione main + controllo formato
    Quando nella textarea sono già presenti i placeholder [cit], appare un modal che gli chiede conferma.
    Aggiunta commenti nel main
    Aggiunta controllo selezione formato
    Tradotto il testo di main js

--- main.js
+++ main.js
@@ -4,20 +4,22 @@ import cerca_cit from "./function_js/cerca_[cit].js";
 import cerca_1 from "./function_js/cerca_[1].js";
 import switch_cit from "./function_js/switch_[cit].js";
 
-let compute_placeholder = document.querySelector("#comp_placeholder");
-let container_provided_by_user_elements =
-  document.querySelector("#ct_prov_by_user");
 
-let radios = document.querySelectorAll(".form-check-input");
-let textarea = document.querySelector("#first_ta");
+//----------------------------Dichiarazione VAR----------------------------
+let compute_placeholder = document.querySelector("#comp_placeholder"); //pulsante Compute_PlaceHolder
+let container_provided_by_user_elements = document.querySelector("#ct_prov_by_user");
 
-let getref = document.querySelector("#getref");
+let textarea = document.querySelector("#first_ta"); //textArea input testo
+let radios = document.querySelectorAll(".form-check-input");//radios per scelta Scopus/Provided By User
+let dropdown = document.querySelector("#dropdownSelect");//dropDown per scelta APA o BibTex se sceglie ProvByUsr
+
+//si possono togliere
 let usr_ref = document.querySelector("#usr_ref");
 let div_getref = document.querySelector("#div-getref");
 
-let dropdown = document.querySelector("#dropdownSelect");
+let getref = document.querySelector("#getref"); //pulsante Get Citation
 
-let result = document.querySelector("#result");
+let result = document.querySelector("#result"); //area del risultato
 
 // abilito tooltip
 const tooltipTriggerList = document.querySelectorAll(
@@ -27,28 +29,75 @@ const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
 
-//l'utente ha cliccato compute placeholder (vengono aggiunti i cit) e lo segnalo
-compute_placeholder.addEventListener("click", () => {
-  //al click di place holder
 
-  if (!cerca_cit(textarea)) {
-    textarea.value += "[cit]";
-  } else {
-    //gestione errore [cit] già presenti
-    let div = document.createElement("div");
-    div.classList.add("alert", "alert-danger");
-    div.setAttribute("role", "alert");
-    div.innerHTML = `
-      ci sono già i cit
-    `;
-    textarea.insertAdjacentElement("afterend", div);
-    setTimeout(() => {
-      div.remove();
-    }, 2000);
+
+//----------------------------Gestione pulsante ComputePlaceholder----------------------------
+//Creo l'HTML del modal
+const modalHTML = `
+<div class="modal fade" id="err_modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">[cit] already inserted</h1>
+        </div>
+      <div class="modal-body">
+        Do you want to replace the [cit]?
+      </div>
+      <div class="modal-footer">
+        <button type="button" id="close" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" id="FC">Yes</button>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+let myModal; //var per gestire il modal
+
+// GESTISCO CLICK PLACEHOLDER: caso con o senza [cit] presenti
+compute_placeholder.addEventListener("click", () => { //al click di place holder
+
+  if (!cerca_cit(textarea)) { //se non sono presenti [cit]
+    textarea.value += "[cit]"; //li aggiungo(chiamata API)
+  } else {//gestione errore [cit] già presenti
+
+    // Prima di aggiungere un nuovo modal, rimuoviamo il modal precedente (se esiste)
+    let existingModal = document.getElementById('err_modal');
+    if (existingModal) {
+      existingModal.remove(); // Rimuovi il modal esistente dal DOM
+    }
+
+    // Aggiungi il modal dinamicamente al body
+    document.body.insertAdjacentHTML('beforeend', modalHTML);
+
+    // Seleziona il modal appena aggiunto
+    myModal = new bootstrap.Modal(document.getElementById('err_modal'));
+
+    // Mostra il modal
+    myModal.show();
+
+    // Assegna l'evento al bottone "Save changes"
+    let btn_FC = document.getElementById('FC');
+    btn_FC.addEventListener('click', () => { //al click del bottone FaiComunque 
+
+      textarea.value += "[cit]"; //Chiamo l'API 
+
+      // Chiudi il modal e rimuovilo dal DOM
+      myModal.hide();
+      document.getElementById('err_modal').remove(); // Rimuovi il modal dal DOM
+    });
+
+    //Gestisco la chiusura tramite il pulsante "Close" 
+    let closeBtn = document.getElementById('close');
+    closeBtn.addEventListener('click', () => {
+      myModal.hide();
+      document.getElementById('err_modal').remove(); // Rimuovi il modal dal DOM
+    });
   }
+
 });
 
-// Verifico se i radio button status e prov cambiano per far apparire la sezione per l'inserimento
+
+//---------------------------- Verifico se i radio button status e prov cambiano per far apparire la sezione per l'inserimento----------------------------
 radios.forEach((radio, index) => {
   radio.addEventListener("change", () => {
     if (index === 0) {
@@ -59,22 +108,47 @@ radios.forEach((radio, index) => {
   });
 });
 
-getref.addEventListener("click", () => {
-  //al click di get citation
 
-  if (cerca_cit(textarea) || cerca_1(textarea)) {
+//---------------------------- Gestione pulsante Get Citation ---------------------------- 
+
+getref.addEventListener("click", () => {//al click di get citation
+
+  if (cerca_cit(textarea) || cerca_1(textarea)) { //se sono presenti [cit] o [1]
+
     let format = dropdown.value;
+
+
     // se seleziona provided by user
     if (radios[1].checked) {
+      
+      if (format!= 0 && format!=1)
+      {
+        //formato non selezionato
+        {
+          let div = document.createElement("div");
+        div.classList.add("alert", "alert-danger");
+        div.setAttribute("role", "alert");
+        div.innerHTML = `
+          You have not chosen the format!
+        `;
+        textarea.insertAdjacentElement("afterend", div);
+        setTimeout(() => {
+          div.remove();
+        }, 2000);
+        
+        }
+      }
+      else{
+      //WARNING PARSING FALLITO  (facciamo quando abbiamo l'API se non restituisce nulla)
       result.innerHTML = `
                 <div class="col-8" >${switch_cit(textarea.value)} </div>
                 <div class="col-4 d-flex align-items-center justify-content-center">
-                    <img src=${
-                      format === 1
-                        ? "./assets/APA_icon.jpeg"
-                        : "./assets/Bibtex_icon.jpg"
-                    } alt="" class="format-logo mb-2">
+                    <img src=${format === 1
+          ? "./assets/APA_icon.jpeg"
+          : "./assets/Bibtex_icon.jpg"
+        } alt="" class="format-logo mb-2">
                 </div>`;
+
       //abilito i popover
       const popoverTriggerList = document.querySelectorAll(
         '[data-bs-toggle="popover"]'
@@ -82,18 +156,21 @@ getref.addEventListener("click", () => {
       let titles = extract_titles(format);
 
       popoverTriggerList.forEach((popoverTriggerEl, index) => {
-          const popover = new bootstrap.Popover(popoverTriggerEl, {
+        const popover = new bootstrap.Popover(popoverTriggerEl, {
           html: true,
           trigger: "focus",
           content: titles[index]
 
         });
       })
-      titles.forEach((title) => {});
+      titles.forEach((title) => { });
     }
-    //selezionato status per GetRef
-    if (radios[0].checked) {
-      // se seleziona status
+    }
+
+
+
+    //selezionato Scopus per GetRef
+    else if (radios[0].checked) {
 
       fetch("risultato.json") // prendo dal file JSON i titoli
         .then((response) => response.json())
@@ -101,8 +178,14 @@ getref.addEventListener("click", () => {
           print_result(data.citazioni, 1, textarea); //format 1 = APE
         });
     }
-  } else {
-    //gestione errore [cit] mancabtu
+
+   
+  }
+
+  //gestione errore [cit] mancanti  
+  else {
+    
+    //creo l'alert
     let div = document.createElement("div");
     div.classList.add("alert", "alert-danger");
     div.setAttribute("role", "alert");
